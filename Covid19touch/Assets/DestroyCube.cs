e0e45f01eedd62191322f626289f8d2693e93e9a
Adnan Karim <adnan.karim@ucalgary.ca>
2021-02-27 18:05:00 -0700

    Added code to when user touches something, in this case just a cube, the gameobject dissappears

---
+++ Covid19touch/Assets/DestroyCube.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class DestroyCube : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Sphere" )
+        {
+            Destroy(collision.gameObject);
+            Console.Write("Collided!");
+        }
+    }
+}
---
+++ Covid19touch/Assets/NearObjectImplementation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Microsoft;
+using Microsoft.MixedReality.Toolkit.Utilities;
+using Microsoft.MixedReality.Toolkit.Input;
+
+public class NearObjectImplementation : MonoBehaviour, IMixedRealityTouchHandler
+{
+    public GameObject cubeToDestroy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+   public void OnTouchCompleted(HandTrackingInputEventData eventData)
+    {
+    }
+    public void OnTouchStarted(HandTrackingInputEventData eventData)
+    {
+        cubeToDestroy = GameObject.Find("CubeToDestroy");
+        Destroy(cubeToDestroy);
+    }
+    public void OnTouchUpdated(HandTrackingInputEventData eventData) { }
+
+}
