03985e721e4b27c2f3cf6350a3afb5119ce38dfc
Adnan Karim <adnan.karim@ucalgary.ca>
2021-03-20 15:28:26 -0600

    on button click text changes; going to add formula now

--- Covid19touch/Assets/UpdateInfectivityRateForWood.cs
+++ Covid19touch/Assets/UpdateInfectivityRateForWood.cs
@@ -1,13 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using TMPro;
+using Microsoft;
+using Microsoft.MixedReality.Toolkit.Utilities;
+using Microsoft.MixedReality.Toolkit.Input;
+using Microsoft.MixedReality.Toolkit.UI;
+using Microsoft.MixedReality.Toolkit;
+
 public class UpdateInfectivityRateForWood : MonoBehaviour
 {
     GameObject textForWood;
     public GameObject description;
+    public UnityEvent OnButtonClicked;
+    public GameObject tempButton;
     public TextMeshPro textMeshPro;
     private bool tempUpdated = false;
+    UnityEngine.TouchScreenKeyboard keyboard;
+    public static string keyboardText = "";
 
     // Start is called before the first frame update
     void Start()
@@ -17,18 +28,41 @@ public class UpdateInfectivityRateForWood : MonoBehaviour
         description = textForWood.gameObject.transform.GetChild(1).gameObject;
         textMeshPro = description.GetComponent<TextMeshPro>();
         textMeshPro.SetText("hi");
+
+        tempButton = textForWood.gameObject.transform.GetChild(2).gameObject.transform.GetChild(1).gameObject;
+
+        var tempButtonToChange = tempButton.GetComponent<Interactable>();
+        tempButtonToChange.OnClick.AddListener(() => textMeshPro.SetText("button change detected!"));
+
         //textForWood.GetComponent<TextMesh>.textForWood = "Adnan";
     }
+
+
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        //get keyboard text components
+        if (TouchScreenKeyboard.visible == false && keyboard != null)
+        {
+            if (keyboard.status == TouchScreenKeyboard.Status.Done)
+            {
+                keyboardText = keyboard.text;
+                textMeshPro.SetText(keyboardText);
+                keyboard = null;
+            }
+        }
     }
 
-    void updateOnUserChange()
-    {
 
+    public static void AddOnClick(Interactable interactable)
+    {
+        interactable.OnClick.AddListener(() => Debug.Log("sdsdsd"));
+    }
+    void invokeKeyboard()
+    {
+        keyboard = TouchScreenKeyboard.Open("", TouchScreenKeyboardType.Default, false, false, false, false);
     }
 
     double formulaForRate()
