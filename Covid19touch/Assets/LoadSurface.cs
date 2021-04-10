d876f7877070b4149eb3b4e742729b193285f719
Adnan Karim <adnan.karim@ucalgary.ca>
2021-04-09 23:21:32 -0600

    added current time

---
+++ Covid19touch/Assets/LoadSurface.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.MixedReality.Toolkit.Input;
+
+
+public class LoadSurface : MonoBehaviour, IMixedRealitySpeechHandler
+{
+    public void OnSpeechKeywordRecognized(SpeechEventData eventData)
+    {
+        loadSurface(eventData.Command.Keyword);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject.Find("ImageTarget_Wood").SetActive(false);
+       // GameObject.Find("VuforiaContent").transform.GetChild(1).gameObject.SetActive(false);
+        //GameObject.Find("VuforiaContent").transform.GetChild(2).gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void loadSurface(string keyword)
+    {
+        System.Console.Write(keyword);
+        switch(keyword)
+        {
+            case "Wood":
+                //GameObject.Find("ImageTarget_Wood").SetActive(true);
+                //System.Console.Write("Made it here!");
+                break;
+            default:
+                break;
+        }
+    }
+
+
+}
---
+++ Covid19touch/Assets/UpdateSurvivalTimeWood.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using TMPro;
+using Microsoft;
+using Microsoft.MixedReality.Toolkit.Utilities;
+using Microsoft.MixedReality.Toolkit.Input;
+using Microsoft.MixedReality.Toolkit.UI;
+using Microsoft.MixedReality.Toolkit;
+using System;
+
+
+public class UpdateSurvivalTimeWood : MonoBehaviour
+{
+    GameObject textForWood;
+    public GameObject description;
+    public UnityEvent OnButtonClicked;
+    public GameObject tempButton;
+    public GameObject sliderForTemp;
+    public GameObject sliderForHumid;
+    public TextMeshPro textMeshPro;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //GetComponent<TextMesh>().text = "Adnan";
+
+        textForWood = GameObject.Find("SurvivalInfoWood");
+        description = textForWood.gameObject.transform.GetChild(1).gameObject;
+        tempButton = textForWood.gameObject.transform.GetChild(2).gameObject.transform.GetChild(1).gameObject;
+
+        var tempButtonToChange = tempButton.GetComponent<Interactable>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        textMeshPro = description.GetComponent<TextMeshPro>();
+        DateTime currentTime = DateTime.Now;
+        textMeshPro.SetText(currentTime.ToString());
+
+    }
+}
